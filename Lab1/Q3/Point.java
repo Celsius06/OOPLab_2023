91aa171d3694072ffbb312c097f7fb559aa7210e
Hidro.nthn <98211917+Celsius06@users.noreply.github.com>
2023-12-06 18:31:24 +0700

    Create Point.java

---
+++ Lab1/Q3/Point.java
@@ -0,0 +1,15 @@
+package Q3;
+
+public class Point { //2 attributes of a point
+    private double x;
+    private double y;
+
+    public Point(double x, double y) { //Take the input from the user
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point target) {
+        return Math.sqrt(Math.pow(this.x - target.x, 2) + Math.pow(this.y - target.y, 2));
+    }
+}
